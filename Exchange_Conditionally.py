b3c773e53f5bd7cceae45c4566251d19ce96142a
Noureddine Achbili <146972598+N0urDEV@users.noreply.github.com>
2023-10-08 21:45:19 +0100

    Add files via upload

---
+++ Exchange_Conditionally.py
@@ -0,0 +1,18 @@
+a = int(input("enter the first number"))
+b = int(input("enter the second number"))
+temp = int 
+sum = int
+multiplication = int 
+if a >= 0 and b >= 0 or a < 0 and b < 0 :
+    a = temp
+    a = b
+    b = temp
+    print("the two numbers has changed")
+else :
+    sum = a + b
+    multiplication = a * b
+    a = sum
+    b = multiplication
+    print("The two numbers do not have the same sign. The sum was put in the first number, and the multiplication was put in the second number")
+print("the first number now is", a)
+print("the second number now is", b)
